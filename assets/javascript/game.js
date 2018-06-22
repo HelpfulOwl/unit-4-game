70660a88cedcb0c50194d5b6063ad94774ccfc11
Elyam <gregory.j.may@gmail.com>
2018-06-22 16:14:24 -0400

    6/22/18

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,86 +2,74 @@ var imgID = ["#fett", "#emperor", "#ewok", "#grievous","#quigon","#solo","#yoda"
 console.log("Array of images id "+imgID);
    
 
-// function clickHandler(event) {
-//     console.log(event)
-// } ;
+
 
 var fett ={
-    health:"100",
-    power: "10"
+    health: 100,
+    power: 10,
 };
 
 var emperor = {
-    health: "80",
-    power: "20"
+    health: 80,
+    power: 20,
 };
+
+var ewok = {
+    health: 70,
+    power: 5,
+};
+
+var grievous = {
+    health: 120,
+    power: 15,
+}
 var charSelected = false;
 var enemySelected = false;
-// function selector(value){
-//     $(imgID[value]).on("click",function(){
-//         $(imgID[value]).detach().appendTo('#row3');
-//         charSelected = true;
-//         console.log(charSelected);
-//     });
-// }
-function toEnemySelect(){
-    $(".characters").detach().appendTo("#row3");
-}
+
+
+
+
 
 $("#char-select").on("click", ".characters",function(){
-    console.log("INSIDE THIS CLICK EVENT!");
-    console.log("THIS: "+ $(this).attr("id"));
+    // console.log("INSIDE THIS CLICK EVENT!");
+    // console.log("THIS: "+ $(this).attr("id"));
     $(this).removeClass("characters");
     $(this).detach().appendTo("#chosen");
-    console.log($(this).attr("class"));
+    //console.log($(this).attr("class"));
     charSelected = true;
+     
+    console.log(yourChar);
     toEnemySelect();
     $("#row2").hide();
+    enemySelect();
+
     
  //console.log(charSelected);
 });
 
 
 function enemySelect() {
-    $("#enemy-select").on("click", ".characters",function(){
-        // $(this).removeClass("characters");
+    $("#row3").on("click", ".characters",function(){
+        $(this).removeClass("characters");
         $(this).detach().appendTo("#enemy");
-        console.log(this);
+        // console.log(this);
         // console.log($(this).attr("class"));
         enemySelected = true;
+        $("#row3").off("click");
     });    
- //console.log(charSelected);
 };
 
-if (charSelected === "true"){
-    enemySelect();
+fight();
+
+function toEnemySelect(){
+    $(".characters").detach().appendTo("#row3");
 }
 
+function fight(){
+    $("#attack").on("click", function(){
+        console.log("Attack!");
+        
 
+    });  
+}
 
-
-
-
-    // $(imgID[0]).on("click",function(){
-    //     $(imgID[0]).detach().appendTo('#row3');
-    //     charSelected = true;
-    //     console.log(charSelected);
-    // });
-
-    // $(imgID[1]).on("click",function(){
-    //     $(imgID[1]).detach().appendTo('#row3');
-    //     charSelected = true;
-    // });
-
-    // $(imgID[2]).on("click",function(){
-    //     $(imgID[2]).detach().appendTo('#row3');
-    //     charSelected = true;
-    // });
-
-    // $(imgID[3]).on("click",function(){
-    //     $(imgID[3]).detach().appendTo('#row3');
-    //     charSelected = true;
-    // });
-
-
-// $('.press-me').on('click', clickHandler)
