7b865cdfefe92f4b9503fc00b19c10abc940b41c
Elyam <gregory.j.may@gmail.com>
2018-06-24 09:08:18 -0400

    6/24/2018

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,33 @@
-var imgID = ["#fett", "#emperor", "#ewok", "#grievous","#quigon","#solo","#yoda"];
-console.log("Array of images id "+imgID);
-   
+var chars = {
+        fett: {
+            health: 100,
+            power: 10,
+        },
 
 
+        emperor: {
+            health: 80,
+            power: 20,
+        },
 
-var fett ={
-    health: 100,
-    power: 10,
-};
 
-var emperor = {
-    health: 80,
-    power: 20,
-};
+        ewok: {
+            health: 70,
+            power: 5,
+        },
 
-var ewok = {
-    health: 70,
-    power: 5,
-};
-
-var grievous = {
-    health: 120,
-    power: 15,
-}
+        grievous: {
+            health: 120,
+            power: 15,
+        },
+    }        
 var charSelected = false;
 var enemySelected = false;
-
-
+var heroIselected;
+var enemyIselected;
+var hHealth;
+var win = false;
+var loss = false;
 
 
 
@@ -36,40 +37,69 @@ $("#char-select").on("click", ".characters",function(){
     $(this).removeClass("characters");
     $(this).detach().appendTo("#chosen");
     //console.log($(this).attr("class"));
+    //console.log($(this).attr("data-name"));
     charSelected = true;
-     
-    console.log(yourChar);
+    var name = $(this).attr("id");
+    console.log(this);
+    console.log(name);
+    heroIselected = chars[name];
+    console.log(heroIselected);
+    
     toEnemySelect();
     $("#row2").hide();
     enemySelect();
-
-    
- //console.log(charSelected);
+    fight();
 });
+
 
 
 function enemySelect() {
     $("#row3").on("click", ".characters",function(){
         $(this).removeClass("characters");
         $(this).detach().appendTo("#enemy");
-        // console.log(this);
+        //console.log(this);
         // console.log($(this).attr("class"));
         enemySelected = true;
         $("#row3").off("click");
+        var name = $(this).attr("id");
+        console.log(this);
+        console.log(name);
+        enemyIselected = chars[name];
+        console.log(enemyIselected);
     });    
 };
 
-fight();
 
 function toEnemySelect(){
-    $(".characters").detach().appendTo("#row3");
-}
+    $(".characters").appendTo("#row3");
+};
 
 function fight(){
     $("#attack").on("click", function(){
-        console.log("Attack!");
+        
+        // console.log(heroIselected);
+        // console.log(enemyIselected);
+        // console.log(heroIselected.health);
+            
+            var hHealth = heroIselected.health - enemyIselected.power;
+            var eHealth = enemyIselected.health - heroIselected.power;
+            heroIselected.health = hHealth;
+            enemyIselected.health = eHealth;
+            console.log(heroIselected.health);
+            console.log(enemyIselected.health);
+            
+            if (eHealth === 0 && hHealth > 0){
+                console.log("You win!");
+                win = true;
+            }
+        
+        //need to figure out how to change the displayed health under image.
+        
+        
+        
+        
         
 
     });  
-}
+};
 
